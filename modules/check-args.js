aa437d9dc02597bcfde4f896d3621e5d893d7bf2
Artem <gretskovart@gmail.com>
2020-10-02 18:03:25 +0400

    fix: implement correct flags checking

--- modules/check-args.js
+++ modules/check-args.js
@@ -3,17 +3,23 @@ const { stderr } = process;
 function checkArgs({ action, shift }) {
   if (!action) {
     stderr.write('Action flag is empty!\n');
-  } else if (action !== 'encode' && action !== 'decode') {
+    process.exit(1);
+  }
+
+  if (action !== 'encode' && action !== 'decode') {
     stderr.write('Action value is incorrect! Must be "encode" or "decode".\n');
+    process.exit(1);
   }
 
   if (!shift) {
     stderr.write('Shift flag is empty!\n');
-  } else if (typeof shift !== 'number' || !Number.isInteger(shift)) {
-    stderr.write('Shift value is incorrect! Must be a positive integer.\n');
+    process.exit(1);
   }
 
-  process.exit(1);
+  if (typeof shift !== 'number' || !Number.isInteger(shift)) {
+    stderr.write('Shift value is incorrect! Must be a positive integer.\n');
+    process.exit(1);
+  }
 }
 
 module.exports = {
